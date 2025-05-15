0f822ce8dab061c4c2ad9f2ecf1e64f77cf83bef
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2025-05-15 17:14:28 -0300

    Início do repositório

---
+++ src/test/java/br/com/hospital/GestaoHospitalar/GestaoHospitalarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hospital.GestaoHospitalar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoHospitalarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
