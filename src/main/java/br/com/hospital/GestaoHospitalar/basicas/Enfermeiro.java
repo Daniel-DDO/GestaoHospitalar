b25687dbef35d4bdbfe7099d01b6451d7e623704
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2025-05-20 14:40:57 -0300

    Atualizar o Enfermeiro.java

--- src/main/java/br/com/hospital/GestaoHospitalar/basicas/Enfermeiro.java
+++ src/main/java/br/com/hospital/GestaoHospitalar/basicas/Enfermeiro.java
@@ -1,4 +1,9 @@
 package br.com.hospital.GestaoHospitalar.basicas;
 
-public class Enfermeiro {
+public class Enfermeiro extends Pessoa {
+  
+  private String corem;
+  private String categoriaEnfermeiro;
+  private List<String> cursosComplementares;
+  
 }
