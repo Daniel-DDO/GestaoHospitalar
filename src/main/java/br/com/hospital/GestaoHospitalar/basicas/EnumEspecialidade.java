c38d25b28b195ebd3586d2beffa0bcd2f96c7c71
Daniel DDO <168279390+Daniel-DDO@users.noreply.github.com>
2025-05-20 14:19:34 -0300

    Atualizar o EnumEspecialidade.java

--- src/main/java/br/com/hospital/GestaoHospitalar/basicas/EnumEspecialidade.java
+++ src/main/java/br/com/hospital/GestaoHospitalar/basicas/EnumEspecialidade.java
@@ -1,9 +1,41 @@
 package br.com.hospital.GestaoHospitalar.basicas;
 
 public enum EnumEspecialidade {
-    Cardiologista("Cardiologista"),
-    Dermatologista("Dermatologista"),
-    Pediatra("Pediatra");
+  
+    Cardiologia("Cardiologia"),
+    Dermatologia("Dermatologia"),
+    Pediatria("Pediatria"),
+    Ortopedia("Ortopedia"),
+    Acupuntura("Acupuntura"),
+    Anestesiologia("Anestesiologia"),
+    ClinicaMedica("Clínica Médica"),
+    Endocrinologia("Endocrinologia"),
+    Endoscopia("Endoscopia"),
+    Gastroenterologia("Gastroenterologia"),
+    Geriatria("Geriatria"),
+    Ginecologia("Ginecologia"),
+    Hematologia("Hematologia"),
+    Homeopatia("Homeopatia"),
+    Infectologia("Infectologia"),
+    Mastologia("Mastologia"),
+    Nefrologia("Nefrologia"),
+    Neurocirurgia("Neurocirurgia"),
+    Neurologia("Neurologia"),
+    Nutrologia("Nutrologia"),
+    Oftalmologia("Oftalmologia"),
+    OncologiaClinica("Oncologia Clínica")
+    Otorrinolaringologia("Otorrinolaringologia"),
+    Patologia("Patologia"),
+    Pneumologia("Pneumologia"),
+    Radiologia("Radiologia"),
+    Reumatologia("Reumatologia"),
+    Urologia("Urologia"),
+    CirurgiaGeral("Cirurgia Geral"),
+    CirurgiaOncologica("Cirurgia Oncológica"),
+    CirurgiaPediatrica("Cirurgia Pediátrica"),
+    CirurgiaPlastica("Cirurgia Plástica"),
+    CirurgiaToracica("Cirurgia Torácica"),
+    CirurgiaVascular("Cirurgia Vascular");
 
     private final String descricao;
 
